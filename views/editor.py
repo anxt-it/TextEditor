3ffd43bef37ab52fa8a421070d9305adbac42f33
anxt-it <anataorevi123@gmail.com>
2026-03-07 19:41:28 +0200

    change back to home button

--- views/editor.py
+++ views/editor.py
@@ -1,6 +1,8 @@
 
 import customtkinter as ctk
 import os.path
+
+from maybe_irrelevant.practice_main import MainMenu
 from services.file_manager import save_file
 
 class Editor(ctk.CTkFrame):
@@ -14,15 +16,28 @@ class Editor(ctk.CTkFrame):
     def create_widgets(self):
         from views.new_file_menu import NewFileMenu
 
-        self.title_entry = ctk.CTkEntry(self, border_color='white', fg_color='transparent', placeholder_text='New Page', font=self.controller.fonts["heading_one"])
+        self.title_entry = ctk.CTkEntry(self, border_color='white',
+                                        fg_color='transparent',
+                                        placeholder_text='New Page',
+                                        font=self.controller.fonts["heading_one"])
 
         self.textbox = ctk.CTkTextbox(self, font=('SF Display', 17))
 
-        self.back_button = ctk.CTkButton(self, text='back', font=self.controller.fonts["small_button"], command=lambda: self.controller.show_frame(NewFileMenu), anchor='e')
-        self.save_button = ctk.CTkButton(self, text='save', font=self.controller.fonts["small_button"], command=save_file, anchor='e')
+        self.home_button = ctk.CTkButton(self, text='home',
+                                         font=self.controller.fonts["small_button"],
+                                         command=lambda: self.controller.show_frame(MainMenu),
+                                         anchor='e')
+        self.save_button = ctk.CTkButton(self, text='save',
+                                         font=self.controller.fonts["small_button"],
+                                         command=save_file,
+                                         anchor='e')
 
-        self.status_bar = ctk.CTkLabel(self, text='Ready   ', font=('SF Display', 12), anchor='w')
-        self.filepath_status = ctk.CTkLabel(self, text="   no file path yet",font=('SF Display', 12), anchor='e')
+        self.status_bar = ctk.CTkLabel(self, text='Ready   ',
+                                       font=('SF Display', 12),
+                                       anchor='w')
+        self.filepath_status = ctk.CTkLabel(self, text="   no file path yet",
+                                            font=('SF Display', 12),
+                                            anchor='e')
 
     def create_layout(self):
         self.columnconfigure(0, weight=1)
@@ -31,7 +46,7 @@ class Editor(ctk.CTkFrame):
 
         self.title_entry.grid(row=0, column=0, padx=100, sticky='swe')
         self.textbox.grid(pady=10, padx=100, sticky='nsew')
-        self.back_button.grid(row=0, column=0, pady=10, sticky='ne')
+        self.home_button.grid(row=0, column=0, pady=10, sticky='ne')
         self.save_button.grid(row=0, column=0, pady=40, sticky='ne')
 
         self.status_bar.grid(row=1, column=0, sticky='se')
